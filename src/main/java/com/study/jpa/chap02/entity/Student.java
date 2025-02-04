7acaad40dd57bcb4ac61a0b59096499535a4de10
chchch928 <kbanda@naver.com>
2025-02-04 14:38:50 +0900

    #0204-01 JPA Query Method

---
+++ src/main/java/com/study/jpa/chap02/entity/Student.java
@@ -0,0 +1,29 @@
+package com.study.jpa.chap02.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+@Getter @Setter @ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+@Entity
+@Table(name = "tbl_student")
+public class Student {
+
+    @Id
+    @Column(name = "stu_id")
+    private String id;
+
+    @Column(name ="stu_name", nullable = false)
+    private String name;
+
+    private String city;
+
+    private String major;
+}
