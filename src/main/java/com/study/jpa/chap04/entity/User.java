2617453dbc850283082d9923730eacc368abedfc
chchch928 <kbanda@naver.com>
2025-02-05 15:49:06 +0900

    #0205-02 JPA 다대다 연관관계

---
+++ src/main/java/com/study/jpa/chap04/entity/User.java
@@ -0,0 +1,31 @@
+package com.study.jpa.chap04.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+// 회원 엔터티
+@Entity
+@Table(name = "tbl_mtm_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "user_name")
+    private String name;
+
+    @OneToMany(mappedBy = "user",cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Purchase> purchases;
+}
